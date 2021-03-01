55a6ff7630ddf86ee8703331926351a1ea1b9476
minjae <alswo8138@gmail.com>
2021-03-01 11:44:54 +0900

    Modify

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const canvas = document.getElementById("jsCanvas")
 const ctx = canvas.getContext("2d")
 const colors = document.getElementsByClassName("control_color");
 const fill = document.getElementById("jsMode");
-const brush = document.getElementById("jsRange")
+const brush = document.getElementById("jsRange");
+const save = document.getElementById("jsSave");
 
 const INITIAL_COLOR = "#2c2c2c";
 const CANVAS_SIZE = 500;
@@ -10,6 +11,8 @@ const CANVAS_SIZE = 500;
 canvas.width = CANVAS_SIZE;
 canvas.height = CANVAS_SIZE;
 
+ctx.fillStyle = "white";
+ctx.fillRect(0,0,CANVAS_SIZE,CANVAS_SIZE);
 ctx.strokeStyle = INITIAL_COLOR;
 ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
@@ -49,7 +52,7 @@ function changeBrush(event){
     ctx.lineWidth = size;
 }
 
-function clickFill(event){
+function handleFill(event){
     if (filling===false){
         filling = true;
         fill.innerHTML = "Paint"
@@ -65,12 +68,25 @@ function paintAll(event){
     }
 }
 
+function handleCM(event){
+    event.preventDefault();
+}
+
+function handleSave(event){
+    const image = canvas.toDataURL("image/jpeg");
+    const link = document.createElement("a");
+    link.href = image ;
+    link.download = "PaintJS"
+    link.click();
+}
+
 if (canvas){
     canvas.addEventListener("mousemove",onMouseMove)
     canvas.addEventListener("mousedown",startPainting)
     canvas.addEventListener("mouseup",stopPainting)
     canvas.addEventListener("mouseleave",stopPainting)
     canvas.addEventListener("click",paintAll)
+    canvas.addEventListener("contextmenu", handleCM)
 }
 
 Array.from(colors).forEach(color => 
@@ -82,7 +98,11 @@ if (brush){
 }
 
 if (fill) {
-    fill.addEventListener("click",clickFill);
+    fill.addEventListener("click",handleFill);
+}
+
+if (save) {
+    save.addEventListener("click",handleSave);
 }
 
 
